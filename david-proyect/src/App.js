9dd7687becb51274853d54a0335bbc9bfd003ff1
Lypher <acosta.enzo@hotmail.com>
2023-05-10 10:34:47 -0300

    fixed

--- david-proyect/src/App.js
+++ david-proyect/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Boxes from "./components/Boxes";
+
 import Landing from "./components/Landing";
 
 function App() {
--- david-proyect/src/components/Navbar.jsx
+++ david-proyect/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import ContactUs from "./ContactUs";
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Ultravity</h1>
-      <ContactUs />
+    <div className="bg-gray-800 text-gray-100">
+      <div className="flex items-center justify-between px-4 py-3">
+        <h1 className="text-xl font-bold">Found Protocol</h1>
+        <ContactUs />
+      </div>
     </div>
   );
 }
