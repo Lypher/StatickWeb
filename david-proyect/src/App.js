fe366f2700608ad83777695b10a073eee19329b5
Lypher <acosta.enzo@hotmail.com>
2023-05-10 10:28:20 -0300

    2

--- david-proyect/src/App.js
+++ david-proyect/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import Boxes from "./components/Boxes";
+import Landing from "./components/Landing";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <Landing />
       <Boxes />
---
+++ david-proyect/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import ContactUs from "./ContactUs";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Ultravity</h1>
+      <ContactUs />
+    </div>
+  );
+}
